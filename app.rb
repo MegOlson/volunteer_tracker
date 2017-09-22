0e8c149147310d4726e68656fb331beee8f08e11
mo <meganannetteolson@yahoo.com>
2017-09-22 10:33:01 -0700

    pass integration spec for adding and displaying project item

--- app.rb
+++ app.rb
@@ -8,39 +8,24 @@ require("pg")
 DB = PG.connect({:dbname => "volunteer_tracker_test"})
 
 get('/') do
+  @projects = Project.all()
   erb(:index)
 end
 
-get('/projects') do
+post('/') do
+  project_title = params.fetch("project_title")
+  project = Project.new({:title => project_title, :id => nil})
+  project.save()
   @projects = Project.all()
-  erb(:projects)
+  erb(:index)
 end
-#
-# get('/projects/new') do
-#   erb(:doctor_form)
-# end
-#
-# get('/doctors/:id') do
-#   @doctor = Doctor.find(params.fetch("id").to_i())
-#   erb(:doctor)
-# end
-#
-# post('/doctors') do
-#   doctor_name = params.fetch("doctor_name")
-#   speciality_type = params.fetch("speciality")
-#   speciality = Speciality.new({:type => speciality_type, :id => nil})
-#   speciality.save()
-#   doctor = Doctor.new({:name => doctor_name, :id => nil, :spec_id => speciality.id()})
-#   doctor.save()
-#   erb(:success)
-# end
-#
+
 # post('/patients') do
 #   patient_name = params.fetch("patient_name")
 #   birthday = params.fetch("birthday")
-#   doctor_id = params.fetch("doctor_id").to_i
-#   @doctor = Doctor.find(doctor_id)
-#   @patient = Patient.new({:name => patient_name, :birthday => birthday, :doctor_id => doctor_id})
+#   project_id = params.fetch("project_id").to_i
+#   @project = Doctor.find(project_id)
+#   @patient = Patient.new({:name => patient_name, :birthday => birthday, :project_id => project_id})
 #   @patient.save()
 #   erb(:success)
 # end
