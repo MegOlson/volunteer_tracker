0d2282c6ad23e4455765e63aa8456aa204b99604
mo <meganannetteolson@yahoo.com>
2017-09-22 08:47:37 -0700

    pass test to return all volunteers in volunteers

--- lib/volunteer.rb
+++ lib/volunteer.rb
@@ -1,10 +1,38 @@
 class Volunteer
-  attr_reader(:id, :name, :project_id)
+  attr_reader(:name, :project_id)
 
   def initialize(attributes)
-    @id = attributes.fetch(:id)
     @name = attributes.fetch(:name)
     @project_id = attributes.fetch(:project_id)
   end
-  
+
+  def self.all
+    returned_volunteers = DB.exec("SELECT * FROM volunteers;")
+    volunteers = []
+    returned_volunteers.each do |volunteer|
+      name = volunteer.fetch("name")
+      project_id = volunteer.fetch("project_id").to_i
+      volunteers.push(Volunteer.new({:name => name, :project_id => project_id}))
+    end
+    volunteers
+  end
+
+  def save
+    DB.exec("INSERT INTO volunteers (name, project_id) VALUES ('#{@name}', #{@project_id});")
+  end
+
+  def ==(another_volunteer)
+    self.name == another_volunteer.name && self.project_id == another_volunteer.project_id
+  end
+
+  def self.find(id)
+   found_volunteer = nil
+   Volunteer.all().each() do |volunteer|
+     if volunteer.id().==(id)
+       found_volunteer = volunteer
+     end
+   end
+   found_volunteer
+  end
+
 end
--- spec/project_spec.rb
+++ spec/project_spec.rb
@@ -81,7 +81,7 @@ describe Project do
       expect(project.title).to eq 'Teaching Ruby to Kids'
     end
   end
-  
+
   context '#delete' do
     it 'allows a user to delete a project' do
       project = Project.new({:title => 'Teaching Kids to Code', :id => nil})
